4c05854d22f1bb3d377c6609a0f516caa8a2f10e
Satyam <satyam7991@gmail.com>
2017-10-24 11:14:01 +0530

    check-seat enabled

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ const API_KEY = '1mfg12275c';
 const ROOT_URL = 'http://api.railwayapi.com/v2/';
 
 export const LIVE_STATUS = 'live_status';
+export const SEAT_STATUS = 'seat_status';
 
 export function liveStatus(trainNum, date) {
 	const url = `${ROOT_URL}live/train/${trainNum}/date/${date}/apikey/${API_KEY}`;
@@ -12,5 +13,15 @@ export function liveStatus(trainNum, date) {
 	return {
 		type : LIVE_STATUS,
 		payload : request
+	}
+}
+
+export function seatAvailability(trainNum, source, dest, date, seatClass, quota) {
+	const url = `${ROOT_URL}check-seat/train/${trainNum}/source/${source}/dest/${dest}/date/${date}/class/${seatClass}/quota/${quota}/apikey/${API_KEY}`;
+	const request = axios.get(url);
+
+	return {
+		type: SEAT_STATUS,
+		payload: request
 	}
 }
--- src/reducers/reducer_services.js
+++ src/reducers/reducer_services.js
@@ -1,9 +1,11 @@
-import { LIVE_STATUS } from '../actions';
+import { LIVE_STATUS, SEAT_STATUS } from '../actions';
 
 export default function ServiceReducer(state={}, action) {
 	switch(action.type) {
 		case LIVE_STATUS :
 			//return action.payload.data;
+			return action.payload.data;
+		case SEAT_STATUS:
 			return action.payload.data;
 		default :
 			//console.log(state);
